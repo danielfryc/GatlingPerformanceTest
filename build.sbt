44569d1efa44340f1a1c523a7ef78a8f3193112d
mkdabrowski <mkdabrowski@gmail.com>
2017-06-04 22:45:38 +0200

    Simulation based on business criteria

--- build.sbt
+++ build.sbt
@@ -2,5 +2,8 @@ enablePlugins(GatlingPlugin)
 
 scalaVersion := "2.11.8"
 
+resolvers += "justwrote" at "http://repo.justwrote.it/releases/"
+
 libraryDependencies += "io.gatling.highcharts" % "gatling-charts-highcharts" % "2.2.5" % "test"
 libraryDependencies += "io.gatling"            % "gatling-test-framework"    % "2.2.5" % "test"
+libraryDependencies += "it.justwrote" %% "scala-faker" % "0.3"
--- src/test/scala/UserSimulation.scala
+++ src/test/scala/UserSimulation.scala
@@ -1,34 +1,64 @@
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
+import faker._
+import scala.util.Random
+
+object User {
+
+  val personFeed = Iterator.continually(
+      Map("PersonName" -> Name.first_name, "PersonAge" -> Random.nextInt(100))
+    )
+
+  val create = repeat(10, "n") {
+        feed(personFeed)
+        .exec(http("Person Create")
+        .post("/persons")
+        .asJSON
+        .body(StringBody("""{ "name": "${PersonName}", "age": ${PersonAge} }"""))
+        .check(status.is(201)))
+        .pause(1) //15 sec
+      }
+
+  val bulk = repeat(10, "n") {
+        feed(personFeed, 10)
+        .exec(http("Person Bulk")
+        .post("/persons/bulk")
+        .asJSON
+        .body(StringBody("""[""" + (1 to 10).map{ i => """{ "name": "${PersonName"""+i+"""}", "age": ${PersonAge"""+i+"""} }"""}.mkString(",") + """]"""))
+        .check(status.is(201)))
+        .pause(1) //150 sec
+      }
+
+  val browse = repeat(5, "n") {
+        exec(session => session.set("page", session.get("n").as[Int] + 1))
+        .exec(http("Person List")
+        .get("/persons/${page}")
+        .check(status.is(200))
+        .check(checkIf(session => session.get("page").as[Int] == 1) {
+            jsonPath("$..name").findAll.saveAs("names")
+          }
+        ))
+        .pause(1) //60 sec
+      }
+
+  val search = repeat(10, "n") {
+        exec(http("Person Search")
+        .get("/persons/name/${names.random()}")
+        .check(status.is(200)))
+        .pause(1) //5 sec
+      }
+}
 
 class UserSimulation extends Simulation {
 
   val httpConf = http
     .baseURL("http://test_perf.reporter02.perf.aws-eu-wi.mel.elt.hosts.pearson-intl.net")
 
-  val scn = scenario("Typical User")
-    .exec(http("Create one Person")
-      .post("/persons")
-      .asJSON
-      .body(StringBody("""{ "name": "Marcin", "age": 99 }"""))
-      .check(status.is(201)))
-    .pause(1)
-    .exec(http("Create bulk Person")
-      .post("/persons/bulk")
-      .asJSON
-      .body(StringBody("""[{ "name": "Marcin_A", "age": 99 }, { "name": "Marcin_B", "age": 99 }]"""))
-      .check(status.is(201)))
-    .pause(1)
-    .exec(http("List Person page")
-      .get("/persons/1")
-      .check(status.is(200)))
-    .pause(1)
-    .exec(http("Search Person")
-      .get("/persons/name/Marcin")
-      .check(status.is(200)))
+  val userScenario = scenario("Typical User")
+    .exec(User.create, User.bulk, User.browse, User.search)
 
-  setUp(scn.inject(rampUsers(1) over (10 seconds))
+  setUp(userScenario.inject(rampUsers(1) over (10 seconds))
     .protocols(httpConf))
     .assertions(global.responseTime.max.lte(2000))  
 }
